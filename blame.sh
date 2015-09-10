62e920a1b3bafe4d47b42ee3b3a2c60f2f5ac0bd
Yan <yans@yancomm.net>
2015-09-09 20:41:06 -0700

    tool for checking line counts

---
+++ blame.sh
@@ -0,0 +1,15 @@
+function blame()
+{
+	PROJ=$1
+	cd $PROJ
+	for f in $(find . -iname "*.py") $(find . -iname "*.c") $(find . -iname *.h) $(find . -iname "*.js") $(find . -iname "*.html")
+	do
+		git blame -w $f 2>/dev/null | cat | sed -e "s/[^(]*(//" | sed -e "s/ .*//"
+	done
+	cd - > /dev/null
+}
+
+for i in "$@"
+do
+	blame $i
+done | sort | uniq -c | sort -n
