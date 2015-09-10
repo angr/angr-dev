782bf85dd31338b3e2ac4112d6032b4b5fa02408
Yan <yans@yancomm.net>
2015-09-09 22:39:05 -0700

    ability to specify the repos to run the command on

--- git_all.sh
+++ git_all.sh
@@ -10,14 +10,28 @@ function red
 	echo "$(tput setaf 1)$@$(tput sgr0)"
 }
 
-for i in */.git/
-do
-	i=${i/\/.git\//}
-	cd $i
-	#green "================================================================================"
-	#green "================================================================================"
+function doit
+{
+	DIR=$1
+	shift
+
+	cd $DIR
 	green "================================================================================"
-	green "=== Running on $i."
-	git $@ && green "=== SUCCESS" || red "=== FAILURE"
+	green "=== Running on $DIR."
+	git "$@" && green "=== SUCCESS" || red "=== FAILURE"
 	cd ..
-done
+}
+
+if [ -n "$REPOS" ]
+then
+	for i in $REPOS
+	do
+		doit $i "$@"
+	done
+else
+	for i in */.git/
+	do
+		i=${i/\/.git\//}
+		doit $i "$@"
+	done
+fi
