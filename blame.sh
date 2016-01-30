1913b46f37b9e2c15481f9f916d734c50796444b
slipper <slipper@sjtu.edu.cn>
2016-01-29 18:22:52 -0800

    let's blame cpp files :)

--- blame.sh
+++ blame.sh
@@ -2,7 +2,7 @@ function blame()
 {
 	PROJ=$1
 	cd $PROJ
-	for f in $(find . -iname "*.py") $(find . -iname "*.c") $(find . -iname *.h) $(find . -iname "*.js") $(find . -iname "*.html")
+	for f in $(find . -iname "*.py") $(find . -iname "*.c*") $(find . -iname *.h) $(find . -iname "*.js") $(find . -iname "*.html")
 	do
 		git blame -w $f 2>/dev/null | cat | sed -e "s/[^(]*(//" | sed -e "s/ .*//"
 	done
