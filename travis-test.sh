e6d32a74bb768999a6ebc40fd82d697fbe83a727
Yan <yans@yancomm.net>
2016-06-16 02:25:23 -0700

    placeholder for travis testing until we port over angr-test

---
+++ travis-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+./git_all.sh checkout master
+./git_all.sh checkout $TRAVIS_BRANCH
+
+if [ "$(basename $TRAVIS_REPO_SLUG)" == "$ANGR_REPO" ]
+then
+	COVERAGE="--with-coverage --cover-package=$ANGR_REPO --cover-erase"
+fi
+
+cd $ANGR_REPO
+if [ -f "test.py" ]
+then
+	nosetests -v --nologcapture --with-timer $COVERAGE test.py
+elif [ -d "tests" ]
+then
+	nosetests -v --nologcapture --with-timer $COVERAGE tests/
+else
+	echo "### No tests for repository $ANGR_REPO?"
+fi
+
+[ "$(basename $TRAVIS_REPO_SLUG)" == "$ANGR_REPO" ] && ../lint.py
