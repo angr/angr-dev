8359ef9249ea78251e9d3938038c204ae6618272
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-24 22:40:35 -0800

    Add bare-bones setup.bat for windows devs

---
+++ setup.bat
@@ -0,0 +1,16 @@
+git clone git@github.com:angr/angr.git
+git clone git@github.com:angr/simuvex.git
+git clone git@github.com:angr/claripy.git
+git clone git@github.com:angr/cle.git
+git clone git@github.com:angr/pyvex.git
+git clone git@github.com:angr/vex.git
+git clone git@github.com:angr/archinfo.git
+git clone git@github.com:angr/capstone.git
+
+pip install -e .\capstone
+pip install -e .\archinfo
+pip install -e .\pyvex
+pip install -e .\cle
+pip install -e .\claripy
+pip install -e .\simuvex
+pip install -e .\angr
