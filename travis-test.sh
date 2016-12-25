ca647e8ee60d0b462286d5440b1efea25f99ee76
Yan <yans@yancomm.net>
2016-12-25 09:35:12 -0800

    allow passing of custom nose options from travis env

--- travis-test.sh
+++ travis-test.sh
@@ -5,7 +5,7 @@
 
 if [ -z "$NO_COVERAGE" -a "$(basename $TRAVIS_REPO_SLUG)" == "$ANGR_REPO" ]
 then
-	NOSE_OPTIONS="--with-coverage --cover-package=$ANGR_REPO --cover-erase"
+	NOSE_OPTIONS="--with-coverage --cover-package=$ANGR_REPO --cover-erase $NOSE_OPTIONS"
 fi
 export NOSE_PROCESS_TIMEOUT=570
 export NOSE_PROCESSES=2
