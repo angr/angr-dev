977f1e60ffe1c362d25fbe4d6b47eb31ef304407
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-25 05:00:13 -0800

    Run multiple tests at once since appveyor doesn't support parallel builds without $$$$

--- appveyor_test.bat
+++ appveyor_test.bat
@@ -1,8 +1,23 @@
-cd %ANGR_REPO% || exit /b 1
+set result=0
+
+:top
+if "%1" == "" goto :end
+echo RUNNING TESTS FOR %1
+cd %1 || exit /b 1
+
 if exist tests (
-  nosetests -v --nologcapture tests || exit /b 1
+  nosetests -v --nologcapture tests || set result=1 && goto :continue
 ) else if exist test.py (
-  nosetests -v --nologcapture test.py || exit /b 1
+  nosetests -v --nologcapture test.py || set result=1 && goto :continue
 ) else (
-  echo 'Unknown test configuration!' && exit /b 1
+  echo Unknown test configuration && set result=1 && goto :continue
 )
+
+:continue
+cd ..
+shift
+echo ------------------------------------------
+goto :top
+
+:end
+exit /b %result%
