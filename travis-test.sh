c5a8090aceafbea5561148bab55019d8ce65b196
Yan <yans@yancomm.net>
2016-12-25 10:53:58 -0800

    do some cleanup before tests, to maximize memory

--- travis-test.sh
+++ travis-test.sh
@@ -10,9 +10,17 @@ echo core | sudo tee /proc/sys/kernel/core_pattern > /dev/null
 #echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 echo 1 | sudo tee /proc/sys/kernel/sched_child_runs_first > /dev/null
 
+# clean up the environment (thanks to https://github.com/dbsrgits/dbix-class/blob/3c26d329/maint/travis-ci_scripts/10_before_install.bash#L5-L15)
+
+sudo /etc/init.d/mysql stop
+sudo /etc/init.d/postgresql stop || /bin/true
+rm -rf /var/ramfs/*
+
+# check out the correct branch
 ./git_all.sh checkout master
 ./git_all.sh checkout $TRAVIS_BRANCH
 
+# run the tests
 if [ -z "$NO_COVERAGE" -a "$(basename $TRAVIS_REPO_SLUG)" == "$ANGR_REPO" ]
 then
 	NOSE_OPTIONS="--with-coverage --cover-package=$ANGR_REPO --cover-erase $NOSE_OPTIONS"
@@ -22,6 +30,7 @@ export NOSE_PROCESSES=${NOSE_PROCESSES-2}
 export NOSE_OPTIONS="-v --nologcapture --with-timer $NOSE_OPTIONS"
 bash -ex ./test.sh $ANGR_REPO
 
+# run lint if necessary
 cd $ANGR_REPO
 if [ "$(basename $TRAVIS_REPO_SLUG)" == "$ANGR_REPO" ]; then
 	echo
