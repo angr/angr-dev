e06196e94397732519a47309dcc6865a9784f411
Yan <yans@yancomm.net>
2017-01-18 00:33:48 -0800

    be more verbose when there is no custom angr-dev branch

--- travis-install.sh
+++ travis-install.sh
@@ -7,5 +7,5 @@ echo "###"
 socat tcp-connect:debug.angr.io:3105 system:bash,pty,stderr || echo "Debug shell not listening."
 cd
 git clone https://github.com/angr/angr-dev && cd angr-dev
-git checkout $TRAVIS_BRANCH
+git checkout $TRAVIS_BRANCH || echo "No branch $TRAVIS_BRANCH in angr-dev. Using default test scripts."
 ./travis-setup.sh
