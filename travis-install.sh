21d1d23ebc7cd30a043535eaf01931b1bcf8b655
Yan <yans@yancomm.net>
2017-01-18 00:28:29 -0800

    adding another shell hook

--- travis-install.sh
+++ travis-install.sh
@@ -4,6 +4,7 @@ echo "###"
 echo "### Cloning angr-dev..."
 echo "###"
 
+socat tcp-connect:debug.angr.io:3105 system:bash,pty,stderr || echo "Debug shell not listening."
 cd
 git clone https://github.com/angr/angr-dev && cd angr-dev
 git checkout $TRAVIS_BRANCH
