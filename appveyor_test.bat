23f00b724a78c35b1fc0f7f4fa40f0e82babc55b
Andrew Dutcher <andrew@andrewdutcher.com>
2017-01-22 17:34:02 -0800

    Set NOSE_PROCESS_RESTARTWORKER for appveyor tests

--- appveyor_test.bat
+++ appveyor_test.bat
@@ -5,6 +5,8 @@ if "%1" == "" goto :end
 echo RUNNING TESTS FOR %1
 cd %1 || exit /b 1
 
+env set NOSE_PROCESS_RESTARTWORKER=1
+
 if exist tests (
   nosetests -v --nologcapture tests || set result=1 && goto :continue
 ) else if exist test.py (
