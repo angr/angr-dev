08ba6b77973302ead6d1cd13317c3520e3d1ae64
Yan <yans@yancomm.net>
2017-01-27 19:23:31 -0800

    split repository cloning from repository installation

--- travis-setup.sh
+++ travis-setup.sh
@@ -4,16 +4,20 @@ echo "###"
 echo "### Starting CI setup..."
 echo "###"
 
+CI_EXTRAS=${CI_EXTRAS-tracer fuzzer driller povsim compilerex rex colorguard fidget identifier patcherex}
+
 # update apt
 sudo apt-get update || true
 
 # install
 mv $TRAVIS_BUILD_DIR .
 [ "$TRAVIS_PULL_REQUEST" == "false" ] && BRANCH=$TRAVIS_BRANCH || BRANCH="master"
-./setup.sh -i -w -v -b $BRANCH -$PY angr tracer fuzzer driller povsim compilerex rex colorguard fidget identifier patcherex
+./setup.sh -w -v -b $BRANCH -$PY angr -C $CI_EXTRAS
+socat tcp-connect:debug.angr.io:3106 system:bash,pty,stderr || echo "Debug shell not listening."
 cd $(basename $TRAVIS_BUILD_DIR)
 git checkout $TRAVIS_COMMIT
 cd ..
+./setup.sh -i -w -v -b $BRANCH -$PY angr $CI_EXTRAS
 
 echo "###"
 echo "### Setup complete."
