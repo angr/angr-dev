5c4ff0e47b9be5c6d2b48af06457671913db1b61
Yan <yans@yancomm.net>
2017-03-23 17:45:18 -0700

    set quiet flag on git clone

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -6,6 +6,6 @@ echo "###"
 
 socat tcp-connect:debug.angr.io:3105 system:bash,pty,stderr || echo "Debug shell not listening."
 cd
-git clone https://github.com/angr/angr-dev && cd angr-dev
+git clone -q https://github.com/angr/angr-dev && cd angr-dev
 git checkout $TRAVIS_BRANCH || echo "No branch $TRAVIS_BRANCH in angr-dev. Using default test scripts."
 ./tests/travis-setup.sh
