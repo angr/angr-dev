256fa9c4daa77237fe87088b9f0f6766c0897739
Yan <yans@yancomm.net>
2017-03-29 01:38:46 -0700

    fix the initial shelling out

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -4,8 +4,8 @@ echo "###"
 echo "### Cloning angr-dev..."
 echo "###"
 
-./tests/shell.sh debug.angr.io 3105
 cd
 git clone -q https://github.com/angr/angr-dev && cd angr-dev
+angr-dev/tests/shell.sh debug.angr.io 3105
 git checkout $TRAVIS_BRANCH || echo "No branch $TRAVIS_BRANCH in angr-dev. Using default test scripts."
 ./tests/travis-setup.sh
