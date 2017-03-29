bc24b722ffd33ad88e653e54b1c065668d42ad3e
Yan <yans@yancomm.net>
2017-03-29 01:52:15 -0700

    actually fix the shell

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -6,6 +6,6 @@ echo "###"
 
 cd
 git clone -q https://github.com/angr/angr-dev && cd angr-dev
-angr-dev/tests/shell.sh debug.angr.io 3105
 git checkout $TRAVIS_BRANCH || echo "No branch $TRAVIS_BRANCH in angr-dev. Using default test scripts."
+./tests/shell.sh debug.angr.io 3105
 ./tests/travis-setup.sh
