7bd3187fa1903ccafe893b23c3fc307abffa1201
Yan <yans@yancomm.net>
2017-05-04 18:49:36 -0700

    install socat...

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -4,6 +4,8 @@ echo "###"
 echo "### Cloning angr-dev..."
 echo "###"
 
+sudo apt-get update && sudo apt-get install -y socat
+
 cd
 git clone -q https://github.com/angr/angr-dev && cd angr-dev
 git checkout $TRAVIS_BRANCH || echo "No branch $TRAVIS_BRANCH in angr-dev. Using default test scripts."
