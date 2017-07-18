7a96e80cf92feaad0f73b51cb35b0cc358f766ea
fmagin <komasa@tfwno.gf>
2017-07-18 07:47:12 +0200

    Minor comfort fixes (#30)
    * Added "--noconfirm" option to the pacman dependency install command which is equivalent to the "-y" for apt-get
    
    * Change the regex that gets ORIGIN_REMOTE so it works with forked repos
    
    * Added "--noconfirm" option to the pacman dependency install command which is equivalent to the "-y" for apt-get
    
    * Change the regex that gets ORIGIN_REMOTE so it works with forked repos
    
    * Added "--noconfirm" option to the pacman dependency install command which is equivalent to the "-y" for apt-get
    
    * Change the regex that gets ORIGIN_REMOTE so it works with forked repos
    
    * Add cleanup script for clean branch switch
    
    * Make clean script executable
    
    * Fix cleanup script to not delete empty files
    
    * Remove cleaning script again

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ declare -A EXTRA_DEPS
 EXTRA_DEPS["simuvex"]="unicorn"
 EXTRA_DEPS["pyvex"]="--pre capstone"
 
-ORIGIN_REMOTE=${ORIGIN_REMOTE-$(git remote -v | grep origin | head -n1 | awk '{print $2}' | sed -e "s|angr/angr-dev.*||")}
+ORIGIN_REMOTE=${ORIGIN_REMOTE-$(git remote -v | grep origin | head -n1 | awk '{print $2}' | sed -e "s|[^/:]*/angr-dev.*||")}
 REMOTES=${REMOTES-${ORIGIN_REMOTE}angr ${ORIGIN_REMOTE}shellphish ${ORIGIN_REMOTE}mechaphish https://git:@github.com/zardus https://git:@github.com/rhelmot https://git:@github.com/salls}
 
 
@@ -180,7 +180,7 @@ then
 			sudo pacman -Syu >>$OUTFILE 2>>$ERRFILE
 		fi
 		info "Installing dependencies..."
-		sudo pacman -S --needed $ARCHDEBS >>$OUTFILE 2>>$ERRFILE
+		sudo pacman -S --noconfirm --needed $ARCHDEBS >>$OUTFILE 2>>$ERRFILE
 	else
 		error "We don't know which dependencies to install for this sytem.\nPlease install the equivalents of these debian packages: $DEBS."
 	fi
