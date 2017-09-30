ca66ba37f0afbdbde863e667e835ddb444d43724
Yan <yans@yancomm.net>
2017-09-30 14:00:09 -0700

    transition

--- admin/blame.sh
+++ admin/blame.sh
@@ -4,7 +4,7 @@ function blame()
 	cd $PROJ
 	for f in $(find . -iname "*.py") $(find . -iname "*.c*") $(find . -iname *.h) $(find . -iname "*.js") $(find . -iname "*.html")
 	do
-		git blame -w $f 2>/dev/null | cat | sed -e "s/[^(]*(//" | sed -e "s/ .*//"
+		git blame -w $f 2>/dev/null | cat | sed -e "s/[^(]*(//" | sed -e "s/ .*//" | sed -e "s/Andrew/Audrey/"
 	done
 	cd - > /dev/null
 }
