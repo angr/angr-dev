f1d3ef2a785afd3bae01d73414a3682e8d54e2f8
Audrey Dutcher <audrey@rhelmot.io>
2019-02-20 00:26:58 -0700

    let's goooooooo

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -4,7 +4,7 @@ echo "###"
 echo "### Starting CI setup..."
 echo "###"
 
-CI_EXTRAS=${CI_EXTRAS-tracer fuzzer driller povsim compilerex rex colorguard fidget patcherex angr-platforms pysoot heaphopper angr-targets}
+CI_EXTRAS=${CI_EXTRAS-tracer fuzzer driller povsim compilerex rex colorguard fidget patcherex angr-platforms pysoot heaphopper angr-targets archr}
 
 # update apt
 echo "Updating apt..."
