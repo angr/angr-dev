0013814354b2b24a7dede1b77c3197827f6717ae
Audrey Dutcher <audrey@rhelmot.io>
2022-05-19 17:37:58 -0700

    Add build_dev_wheels.sh script for snapshotting local environment into a distributable state

---
+++ admin/build_dev_wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+REPO_ROOT="$(realpath "$(dirname $0)/..")"
+
+# if you edit this or specify a new value make sure the packages are in dependency order!
+REPOS=${REPOS-archinfo pyvex claripy cle ailment angr}
+
+OUTDIR=${OUTDIR-./build}
+mkdir -p "$OUTDIR"
+
+python -m pip install -U setuptools wheel build
+export PIP_FIND_LINKS=$OUTDIR
+
+for REPO in $REPOS; do
+	python -m build --wheel --outdir "$OUTDIR" "$REPO_ROOT/$REPO"
+done
+
+echo 'All done!'
+echo "Wheels for $REPOS can be found in $OUTDIR"
