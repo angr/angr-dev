ac89324df50f9257ba605257e479fb57ed51c269
Kevin Phoenix <kevin@kphoenix.us>
2022-10-25 14:16:27 -0700

    Simplify extremely-simple-setup.sh

--- extremely-simple-setup.sh
+++ extremely-simple-setup.sh
@@ -13,8 +13,7 @@ fi
 ## BEGIN VERY COMPLICATED INSTALL PROCEDURE
 
 git clone https://github.com/angr/archinfo.git
-git clone https://github.com/angr/vex.git
-git clone https://github.com/angr/pyvex.git
+git clone --recursive https://github.com/angr/pyvex.git
 git clone https://github.com/angr/claripy.git
 git clone https://github.com/angr/cle.git
 git clone https://github.com/angr/angr.git
@@ -22,54 +21,14 @@ git clone https://github.com/angr/ailment.git
 git clone https://github.com/angr/angr-doc.git
 git clone https://github.com/angr/binaries.git
 
-set -e
+python -m pip install -U pip wheel setuptools "unicorn==1.0.2rc4"
 
 pip install -e ./ailment
 pip install -e ./archinfo
 pip install -e ./pyvex
 pip install -e ./claripy
 pip install -e ./cle
-pip install -e ./angr
+pip install --no-build-isolation -e ./angr
 
 ## END VERY COMPLICATED INSTALL PROCEDURE
 
-pip install nose ipython ipdb
-pip install -I --no-binary keystone-engine keystone-engine
-
-set +e
-
-echo "Do you want to install the extra mechaphish analysis components?"
-echo "This will fail if your machine isn't 64bit x86"
-echo -n "y/N "
-read choice
-if ! [ "$choice" = y -o "$choice" = Y ]; then
-  exit 1
-fi
-
-# BEGIN EVEN MORE COMPLICATED INSTALL PROCEDURE
-
-git clone https://github.com/angr/wheels.git
-git clone https://github.com/mechaphish/colorguard.git
-git clone https://github.com/mechaphish/compilerex.git
-git clone https://github.com/shellphish/driller.git
-git clone https://github.com/shellphish/fuzzer.git
-git clone https://github.com/mechaphish/povsim.git
-git clone https://github.com/shellphish/rex.git
-git clone https://github.com/angr/tracer.git
-git clone https://github.com/salls/angrop.git
-
-set -e
-
-pip install wheels/shellphish_afl*.whl
-pip install wheels/shellphish_qemu*.whl
-
-pip install -e ./angrop
-pip install -e ./tracer
-pip install -e ./driller
-pip install -e ./fuzzer
-pip install -e ./povsim
-pip install -e ./compilerex
-pip install -e ./rex
-pip install -e ./colorguard
-
-# END EVEN MORE COMPLICATED INSTALL PROCEDURE
