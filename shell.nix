d474d5d3807638cc9a40d4dbe604e86f46c167f1
Audrey Dutcher <audrey@rhelmot.io>
2024-08-17 12:44:03 -0700

    Make shell.nix less global-stateful and more documented

--- shell.nix
+++ shell.nix
@@ -1,6 +1,14 @@
+# This shell script sets up a python virtual environment with all the necessary dependencies for angr to run.
+# Its purpose is to generate an environment where you can DEVELOP angr, i.e. have all the code laid out in
+# front of you and be able to edit it. For this end we do some very non-nix things to make it work, creating
+# a python virtualenv in the current directory and shelling out to pip.
+#
+# This shell is meant to be run cloned from the angr-dev repository. You can then use the rest of the angr-dev
+# niceties, such as git_all.sh
+
 with import <nixpkgs> { };
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   name = "angr-env";
 
   nativeBuildInputs = [ cmake pkg-config git ];
@@ -29,6 +37,12 @@ stdenv.mkDerivation rec {
   shellHook = ''
     source $(command -v virtualenvwrapper.sh)
     export LD_LIBRARY_PATH="${lib.getLib stdenv.cc.cc}/lib:$LD_LIBRARY_PATH"
-    workon angr 2>/dev/null || { mkvirtualenv angr && NIX_ENFORCE_PURITY= ./extremely-simple-setup.sh; }
+    if ! [ -d ".venv" ]; then
+      python -m venv .venv
+      source .venv/bin/activate
+      NIX_ENFORCE_PURITY= ./extremely-simple-setup.sh
+    else
+      source .venv/bin/activate
+    fi
   '';
 }
