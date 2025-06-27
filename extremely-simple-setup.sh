582c2dbaf8640ae9190ce73f594dc81ffa3bf71b
Audrey Dutcher <audrey@rhelmot.io>
2025-06-26 18:55:51 -0700

    this settings is not compatible with scikit-build-core

--- extremely-simple-setup.sh
+++ extremely-simple-setup.sh
@@ -11,7 +11,7 @@ git clone https://github.com/angr/binaries.git
 python -m pip install -U pip wheel setuptools setuptools-rust cffi "unicorn==2.0.1.post1"
 
 pip install -e ./archinfo --config-settings editable_mode=strict
-pip install -e ./pyvex --config-settings editable_mode=strict
+pip install -e ./pyvex
 pip install -e ./cle --config-settings editable_mode=strict
 pip install -e ./claripy --config-settings editable_mode=strict
 pip install --no-build-isolation -e ./angr --config-settings editable_mode=compat
