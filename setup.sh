f478f7b2112fa6d1cb2320381ff6bd95e05a5af9
Jonathan Wesley Stone <js@piratejon.com>
2025-07-07 15:16:58 -0500

    Fix -P on arch (except angr-management) (#198)

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ REPOS_CPYTHON=${REPOS_CPYTHON-angr-management}
 if [ `uname` == "Linux" ]; then REPOS="${REPOS} archr"; fi
 declare -A EXTRA_DEPS
 EXTRA_DEPS["angr"]="sqlalchemy unicorn==2.0.1.post1"
-EXTRA_DEPS["pyvex"]="--pre capstone"
+EXTRA_DEPS["pyvex"]="--pre capstone scikit-build-core"
 
 ORIGIN_REMOTE=${ORIGIN_REMOTE-$(git remote -v | grep origin | head -n1 | awk '{print $2}' | sed -e "s|[^/:]*/angr-dev.*||")}
 REMOTES=${REMOTES-${ORIGIN_REMOTE}angr ${ORIGIN_REMOTE}shellphish ${ORIGIN_REMOTE}mechaphish https://git:@github.com/zardus https://git:@github.com/rhelmot https://git:@github.com/salls https://git:@github.com/lukas-dresel https://git:@github.com/mborgerson}
