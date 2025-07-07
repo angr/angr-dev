84f5cfb9e045cacc6538e2643df43c5f1c715fa7
Kevin Phoenix <kevin@kphoenix.us>
2025-07-07 13:14:19 -0700

    Revert "HUH?"
    This reverts commit 87711e909f62e6797a43a73ca7b0da18f0b9debc.

--- extremely-simple-setup.sh
+++ extremely-simple-setup.sh
@@ -8,11 +8,10 @@ git clone https://github.com/angr/angr.git
 git clone https://github.com/angr/angr-management.git
 git clone https://github.com/angr/binaries.git
 
-python -m pip install -U pip wheel setuptools setuptools-rust scikit-build-core cffi "unicorn==2.0.1.post1"
+python -m pip install -U pip wheel setuptools setuptools-rust cffi "unicorn==2.0.1.post1"
 
 pip install -e ./archinfo --config-settings editable_mode=strict
-# LOAD BEARING NOP: --no-build-isolation should do nothing here. however what actually happens is that on the first install it will fail to install one of the headers into the source tree and then subsequent installs will be just fine. whatever state controls this behavior is totally unknown and I have never managed to "clear" this state other than completely nuking the container I'm running the install in.
-pip install --no-build-isolation -e ./pyvex
+pip install -e ./pyvex
 pip install -e ./cle --config-settings editable_mode=strict
 pip install -e ./claripy --config-settings editable_mode=strict
 pip install --no-build-isolation -e ./angr --config-settings editable_mode=compat
