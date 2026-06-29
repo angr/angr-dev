8df8e660162173ea39a81d65839847da40564fc0
Audrey Dutcher <audrey@rhelmot.io>
2026-06-29 12:46:06 -0700

    update nix overlay

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,6 +4,7 @@
   claripy ? pkgs: ../claripy,
   cle ? pkgs: ../cle,
   archinfo ? pkgs: ../archinfo,
+  angr-data ? pkgs: ../angr-data,
 }:
 final: prev:
 let
@@ -16,7 +17,7 @@ in {
           angrRepo = srcSpec: attrs: let
             path = srcSpec final;
             project = pyproject-nix.lib.project.loadPyproject { projectRoot = path; };
-            devVersion = builtins.elemAt (builtins.match ".*__version__ = \"([^\"]*)\".*" (builtins.readFile "${path}/${project.pyproject.project.name}/__init__.py")) 0;
+            devVersion = builtins.elemAt (builtins.match ".*__version__ = \"([^\"]*)\".*" (builtins.readFile "${path}/${attrs.pname or project.pyproject.project.name}/__init__.py")) 0;
             commit = lib.sources.commitIdFromGitRepo "${path}/.git";
             commitShort = lib.strings.substring 0 10 commit;
             pyprojectAttrs = project.renderers.buildPythonPackage {
@@ -28,7 +29,7 @@ in {
           in final'.buildPythonPackage (pyprojectAttrs // extraAttrs // attrs);
         in {
             angr = (angrRepo angr {
-              pythonRelaxDeps = [ "capstone" "lmdb" ];
+              pythonRelaxDeps = [ "capstone" "lmdb" "angr-data" ];
               nativeBuildInputs = [
                 final.rustPlatform.cargoSetupHook
                 final.cargo
@@ -39,7 +40,7 @@ in {
                 lockFile = "${prev.src}/Cargo.lock";
                 outputHashes = {
                   # well this is a nightmare
-                  "icicle-cpu-0.1.0" = "sha256-WGh9U9fOo2VrQS7BFDqJ5TSEyMejZii1v6OuVJqixag=";
+                  "icicle-cpu-0.1.0" = "sha256-dF4ic0r+Z4WXqVIkKpxYfLiBg+i6Ohxy6rAgikJMZew=";
                 };
               };
             });
@@ -64,8 +65,35 @@ in {
               pythonRelaxDeps = [ "arpy" ];
             };
             archinfo = angrRepo archinfo {};
+            angr-data  = angrRepo angr-data {
+              pname = "angr_data";
+            };
             pyxdia = final'.callPackage ./pyxdia.nix { };
             uefi-firmware = final'.uefi-firmware-parser;
+            rust-demangler = final'.callPackage ./rust-demangler.nix { };
+            scikit-build-core = prev'.scikit-build-core.overrideAttrs {
+              version = "0.12.2";
+              src = final.fetchFromGitHub {
+                owner = "scikit-build";
+                repo = "scikit-build-core";
+                rev = "v0.12.2";
+                hash = "sha256-JE6z44u1FLfI+Gguhd2rVUvY8tyEoo/WviGJmPRT8kc=";
+              };
+              patches = [];
+              disabledTestPaths = [
+                "tests/test_editable.py"
+                "tests/test_builder.py"
+              ];
+            };
+            pypcode = prev'.pypcode.overrideAttrs {
+              version = "4.0.0";
+              src = final.fetchFromGitHub {
+                owner = "angr";
+                repo = "pypcode";
+                tag = "v4.0.0";
+                hash = "sha256-OwnwgN2/MElH7SOwauS/hfVkgwAd0uMH0y00Ydkq+8I=";
+              };
+            };
         })
     ];
 }
